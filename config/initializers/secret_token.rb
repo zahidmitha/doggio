a86d6b46cce186744bf9ab069a2c6cad0d0852a6
Zahid <kronosmes@gmail.com>
2013-12-08 00:52:26 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doggio::Application.config.secret_token = 'a4351decbe01b0786b934fc7eff739c9dab052c8f846ba1f7b550ced809be44cc0dc642ec49bab7100f2b7136d27ad97c799b367bb74b2be482eae76a4826ec2'
